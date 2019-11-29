ded7480846535e61cd94adfbdb7956a6e05c1a67
gebhartn <nicholas.gebhart@gmail.com>
2019-11-28 20:20:04 -0800

    chore: comments create function

--- commands/playlists/create.js
+++ commands/playlists/create.js
@@ -7,15 +7,21 @@ module.exports = {
   category: "playlists",
   description: "create a new playlist",
   run: async (client, msg, content) => {
+    // get discord user id
     const creator = parseInt(msg.author.id);
+    // get playlist name
     const name = content.join(" ");
+    // post object
     const body = { creator, name };
+    // confirm command was received
     const message = await msg.channel.send(`Creating new playlist: ${name}`);
 
     if (!name.length) {
+      // return if no playlist name is provided
       message.edit(`ERROR: Must provide a playlist name`);
     } else {
       try {
+        // axios post, returns playlist findBy -> name
         await insertPlaylist(`${baseURL}/${endpoint}`, body);
         message.edit(`Created playlist: ${name}`);
       } catch (err) {

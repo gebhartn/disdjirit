111bde719d0b05e772df92025f7da3cc971ce0e6
gebhartn <nicholas.gebhart@gmail.com>
2019-11-28 20:18:44 -0800

    chore: comments add command

--- commands/playlists/add.js
+++ commands/playlists/add.js
@@ -8,24 +8,34 @@ module.exports = {
   category: "playlists",
   description: "adds song to a playlist",
   run: async (client, msg, content) => {
+    // get discord user id
     const creator = parseInt(msg.author.id);
+    // extract song name and playlist name from message
     const [song, playlist] = joinTrimSplit(content);
+    // send message, confirms command worked
     const message = await msg.channel.send(`Adding ${song} to ${playlist}`);
+    // if no playlist name return error
     if (!playlist) {
       message.edit(`ERROR: Must provide a playlist name`);
     } else {
       try {
+        // query youtube, (song name, number of results, cb)
         searchYoutube(song, 1, (err, res) => {
+          // extract first object from result array
           const [video] = res.items;
+          // video title
           const name = video.snippet.title;
+          // video endpoint
           const url = video.id.videoId;
           if (err) console.log(err);
+          // axios post
           insertSong(`${baseURL}/${endpoint}`, {
             playlist,
             creator,
             name,
             url: `https://www.youtube.com/watch?v=${url}`
           }).then(server => {
+            // post returns boolean based on viable inputs
             server.data
               ? message.edit(`Added ${name} to ${playlist}`)
               : message.edit(`You don't own a playlist called ${playlist}`);
